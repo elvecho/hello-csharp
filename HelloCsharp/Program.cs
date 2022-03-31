0cceba0ee5e2c29c9fb2e17d08eb6034ef21e2e2
elvecho <100934190+elvecho@users.noreply.github.com>
2022-03-31 16:37:19 +0200

    commit

--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("benvenuto");
+Console.WriteLine("gennaio");
+Console.WriteLine("febbraio");
+Console.WriteLine("marzo");
+Console.WriteLine("aprile");
+Console.WriteLine("maggio");
+Console.WriteLine("giugno");
+Console.WriteLine("luglio");
+Console.WriteLine("agosto");
+Console.WriteLine("settembre");
+Console.WriteLine("ottobre");
+Console.WriteLine("novembre");
+Console.WriteLine("dicembre");
+Console.Write("scrivi il tuo mese preferito:");
+var mese = Console.ReadLine();
+Console.WriteLine(mese);
+Console.WriteLine("ottima scelta, alla prossima");
